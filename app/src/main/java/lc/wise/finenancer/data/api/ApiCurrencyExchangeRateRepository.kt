c951eecc602c1243f0d44f7e6ab13e6fe36f4b6a
1itvin <litvin.roman2003@gmail.com>
2024-08-22 15:19:58 +0300

    perf: added cache

--- app/src/main/java/lc/wise/finenancer/data/api/ApiCurrencyExchangeRateRepository.kt
+++ app/src/main/java/lc/wise/finenancer/data/api/ApiCurrencyExchangeRateRepository.kt
@@ -1,19 +1,32 @@
 package lc.wise.finenancer.data.api
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import lc.wise.finenancer.domain.entity.CurrencyExchangeRate
 import lc.wise.finenancer.domain.repository.CurrencyExchangeRateRepository
+import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 
 class ApiCurrencyExchangeRateRepository @Inject constructor(
     private val service: ApiCurrencyExchangeRateService,
     private val mapper: ApiCurrencyExchangeRateMapper
 ) : CurrencyExchangeRateRepository {
+
+    private val cache = ConcurrentHashMap<String, CurrencyExchangeRate>()
+    private val mutex = Mutex()
+
     override suspend fun getExchangeRate(requestedCurrency: String): CurrencyExchangeRate {
         return withContext(Dispatchers.IO) {
-            val entity = service.getExchangeRate(requestedCurrency)
-            mapper.fromEntity(entity)
+            mutex.withLock {
+                cache[requestedCurrency] ?: run {
+                    val entity = service.getExchangeRate(requestedCurrency)
+                    val exchangeRate = mapper.fromEntity(entity)
+                    cache[requestedCurrency] = exchangeRate
+                    exchangeRate
+                }
+            }
         }
     }
 }
--- app/src/main/java/lc/wise/finenancer/domain/interactors/CurrencyExchangeRateInteractor.kt
+++ app/src/main/java/lc/wise/finenancer/domain/interactors/CurrencyExchangeRateInteractor.kt
@@ -5,16 +5,16 @@ import lc.wise.finenancer.domain.repository.CurrencyExchangeRateRepository
 import javax.inject.Inject
 
 class CurrencyExchangeRateInteractor @Inject constructor(
-    private val exchangeRateRepository: CurrencyExchangeRateRepository
+    private val currencyExchangeRateRepository: CurrencyExchangeRateRepository
 ) {
 
     suspend fun getExchangeRate(currency: String): CurrencyExchangeRate {
-        return exchangeRateRepository.getExchangeRate(currency)
+        return currencyExchangeRateRepository.getExchangeRate(currency)
     }
 
     suspend fun convertCurrency(amount: Double, fromCurrency: String, toCurrency: String): Double {
-        val fromExchangeRate = exchangeRateRepository.getExchangeRate(fromCurrency)
-        val toExchangeRate = exchangeRateRepository.getExchangeRate(toCurrency)
+        val fromExchangeRate = currencyExchangeRateRepository.getExchangeRate(fromCurrency)
+        val toExchangeRate = currencyExchangeRateRepository.getExchangeRate(toCurrency)
         return amount * (fromExchangeRate.rate / toExchangeRate.rate) *
             (toExchangeRate.scale / fromExchangeRate.scale)
     }
