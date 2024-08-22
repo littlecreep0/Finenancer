32572710d58cbdf8b46b3da72998fbf063186064
1itvin <litvin.roman2003@gmail.com>
2024-08-22 23:44:20 +0300

    fix: delete mutex

--- app/src/main/java/lc/wise/finenancer/data/api/ApiCurrencyExchangeRateRepository.kt
+++ app/src/main/java/lc/wise/finenancer/data/api/ApiCurrencyExchangeRateRepository.kt
@@ -1,8 +1,6 @@
 package lc.wise.finenancer.data.api
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import lc.wise.finenancer.domain.entity.CurrencyExchangeRate
 import lc.wise.finenancer.domain.repository.CurrencyExchangeRateRepository
@@ -15,17 +13,14 @@ class ApiCurrencyExchangeRateRepository @Inject constructor(
 ) : CurrencyExchangeRateRepository {
 
     private val cache = ConcurrentHashMap<String, CurrencyExchangeRate>()
-    private val mutex = Mutex()
 
     override suspend fun getExchangeRate(requestedCurrency: String): CurrencyExchangeRate {
         return withContext(Dispatchers.IO) {
-            mutex.withLock {
-                cache[requestedCurrency] ?: run {
-                    val entity = service.getExchangeRate(requestedCurrency)
-                    val exchangeRate = mapper.fromEntity(entity)
-                    cache[requestedCurrency] = exchangeRate
-                    exchangeRate
-                }
+            cache[requestedCurrency] ?: run {
+                val entity = service.getExchangeRate(requestedCurrency)
+                val exchangeRate = mapper.fromEntity(entity)
+                cache[requestedCurrency] = exchangeRate
+                exchangeRate
             }
         }
     }
